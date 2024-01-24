e0b1ba2deac29da8d8605de8ad1b5e2484296eeb
vickyrdy <56144588+vickyrdy@users.noreply.github.com>
2024-01-24 10:44:49 +0000

    Update text.js

--- text.js
+++ text.js
@@ -1,12 +1,11 @@
-const messageInput = document.getElementById('message');
-    const counter = document.getElementById('counter');
-    const sendButton = document.getElementById('sendButton');
+const messageInput = document.getElementById("message");
+const counter = document.getElementById("counter");
+const sendButton = document.getElementById("sendButton");
 
-    messageInput.addEventListener('input', updateCounter);
+messageInput.addEventListener("input", updateCounter);
 
-    function updateCounter() {
-      const currentLength = messageInput.value.length;
-      counter.textContent = `${currentLength}/500 characters`;
-      sendButton.disabled = currentLength === 0 || currentLength > 500;
-     sendButton.disabled = currentLength === 0 || currentLength > 500;
-    }}
+function updateCounter() {
+  const currentLength = messageInput.value.length;
+  counter.textContent = `${currentLength}/500 characters`;
+  sendButton.disabled = currentLength === 0 || currentLength > 500;
+}
